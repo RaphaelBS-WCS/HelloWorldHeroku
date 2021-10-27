3bc3de0da97ac41c68bc4786cc04c1ea39b4b132
Niurwa <raphaelbilletservoin@gmail.com>
2021-10-27 15:16:28 +0200

    Initial commit

---
+++ src/test/java/com/wildcodeschool/HelloWorld/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.HelloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
